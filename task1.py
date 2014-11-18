576e022fb0878d60221126509ab0758a2818e30b
Dmitriy Khodakov <dmitryhd@gmail.com>
2014-11-18 14:48:39 +0300

    test added and passed

--- task1.py
+++ task1.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+import unittest
 
 def ip_host(ip_host_f):
     ''' соответствие ip и имени host'''
@@ -10,29 +11,36 @@ def ip_host(ip_host_f):
     #ipPattern = re.compile(r'^(\d+.\d+.\d+.\d+).*[\t|\ ](\w+[.\w]*)$')
     #ipPattern = re.compile(r'^(\d+.\d+.\d+.\d+)([[\t|\ ].*[\t|\ ]]|[\t|\ ])(\w+[\W\w+]*)$')
     ipPattern = re.compile(r'^(\d+.\d+.\d+.\d+).*[\t|\ ](\w+[\W\w+]*)$')
+    res = {}
     with open(ip_host_f) as a_file:
         for a_line in a_file:
             line_number += 1
-            a_rez=ipPattern.match(a_line.rstrip())
+            a_rez = ipPattern.match(a_line.rstrip())
             #a_rez=ipPattern.search('1.2.3.4  \t\t\   gfdfgdfgs sfgsdf')
             try:
-                print('\t',a_rez.groups()[0],'\t',a_rez.groups()[1])
+                res[a_rez.groups()[1]] = a_rez.groups()[0]
             except AttributeError:  # Nothing found.
                 pass
-                #print(len(a_rez.groups()))
-            #print(a_line.rstrip())
-            #print(line_number)
+    return res
 
 
-if len(sys.argv)!=2:
-    print('первый аргумент путь к фаилу')
-else:
-    #f = open(argv[1]).read()
-    #ip_host(f)
-    ip_host(sys.argv[1])
-    
+class TestHostLookup(unittest.TestCase):
+    test_file_name = './data_test/t1.txt'
+    expected_basic = {'localhost': '127.0.0.1'}
+
+    def test_basic(self):
+        res = ip_host(self.test_file_name)
+        assert res == self.expected_basic
 
 
-    
+def main():
+    if len(sys.argv) == 1:
+        unittest.main()
+    elif len(sys.argv) == 2:
+        ip_host(sys.argv[1])
+    else:
+        print('первый аргумент путь к фаилу')
 
 
+if __name__ == '__main__':
+    main()
