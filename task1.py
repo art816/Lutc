6b8e96c4d6bfef1e9c5a209d62416bd91eb35061
Artem Pilkevich <fizmat39reg.bot@mail.ru>
2014-11-24 10:42:56 +0300

    изменения поиска ip, host в строке

--- task1.py
+++ task1.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-#sfgsfgsgsfgf
-#sdfsdf
 
 
 import sys
@@ -8,26 +6,24 @@ import re
 import unittest
 
 
-
-def ip_host(ip_host_f):
-    ''' соответствие ip и имени host'''
+def iphost(ip_host_f):
+    '''выделяем из строки ip и имя host'''
     line_number = 0
-
-    #ippattern = re.compile(r'^(\d+.\d+.\d+.\d+)*(\b\w+)$')
-    #ippattern = re.compile(r'^(\d+.\d+.\d+.\d+).*[\t|\ ](\w+[.\w]*)$')
-    #ippattern = re.compile(r'^(\d+.\d+.\d+.\d+)([[\t|\ ].*[\t|\ ]]|[\t|\ ]) (\w+[\W\w+]*)$')
-
-    ippattern = re.compile(r'^(\d+.\d+.\d+.\d+).*\s(\w+[\W\w+]*)$')
+    #iphost_ipPattern = re.compile(r'^(\d+.\d+.\d+.\d+)*(\b\w+)$')
+    #iphost_ipPattern = re.compile(r'^(\d+.\d+.\d+.\d+).*[\t|\ ](\w+[.\w]*)$')
+    #iphost_ipPattern = re.compile(r'^(\d+.\d+.\d+.\d+)([[\t|\ ].*[\t|\ ]]|[\t|\ ]) # (\w+[\W\w+]*)$')
+    iphost_ipPattern = re.compile(r'^(\d+.\d+.\d+.\d+).*[\s\W]([\w.-]*)$')
     res = {}
     with open(ip_host_f) as a_file:
         for a_line in a_file:
             line_number += 1
-            a_rez = ippattern.match(a_line.rstrip())
+            a_rez = iphost_ipPattern.match(a_line.rstrip())
             try:
                 res[a_rez.groups()[1]] = a_rez.groups()[0]
             except AttributeError:  # Nothing found.
                 pass
     return res
+
 
 class TestHostLookup(unittest.TestCase):
     test_file_names = ['./data_test/t1.txt',
@@ -37,15 +33,15 @@ class TestHostLookup(unittest.TestCase):
 
     def test_basic(self):
         for test_fn, exp in zip(self.test_file_names, self.expected_res):
-            res = ip_host(test_fn)
+            res = iphost(test_fn)
             assert res == exp, 'got:{} exp:{}'.format(res, exp)
-    # a, b = b, a
+
 
 def main():
     if len(sys.argv) == 1:
         unittest.main()
     elif len(sys.argv) == 2:
-        ip_host(sys.argv[1])
+        iphost(sys.argv[1])
     else:
         print('первый аргумент путь к фаилу')
 
