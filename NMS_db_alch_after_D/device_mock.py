825b5e9aa40a1566029c687b2982d908a63dfa76
Artem Pilkevich <fizmat39reg.bot@mail.ru>
2014-12-12 15:53:21 +0300

    созданы классы devices parameters

--- NMS_db_alch_after_D/device_mock.py
+++ NMS_db_alch_after_D/device_mock.py
@@ -2,6 +2,7 @@
 
 """ Mock classes for device and parameter """
 
+import settings as cnf
 
 class Device:
     def __init__(self, name, parameters):
@@ -9,9 +10,10 @@ class Device:
         self.parameters = parameters  # list
 
 class Parameter:
-    def __init__(self, name, value):
+    def __init__(self, name, value, _type):
         self.name = name
         self.value = value
+        self._type = _type
 
 def main():
     par1 = Parameter('param1', 1)
@@ -21,3 +23,7 @@ def main():
     # save to db
     dm_man = DatabaseManager()
     dm_man.save([dev1])
+
+def create_devices():
+    return {dev_name: Device(dev_name, cnf.dev_parameters[dev_name]) for dev_name in cnf.dev_names}
+
