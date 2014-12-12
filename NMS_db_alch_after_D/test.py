8265445c1f20625a5da802f6bce5c524bce5fcd0
Artem Pilkevich <fizmat39reg.bot@mail.ru>
2014-12-12 15:56:41 +0300

    пятая версия добавлены новые тесты

--- NMS_db_alch_after_D/test.py
+++ NMS_db_alch_after_D/test.py
@@ -5,22 +5,62 @@
 __author__ = 'art'
 
 import unittest
-from sqlalchemy import create_engine
-# from sqlalchemy import *
-import db_manager
-# db_manager
-import settings as cnf
 import os
-from sqlalchemy import select
+import time
+from sqlalchemy import create_engine, select, Integer
+
+import db_manager
+import settings as cnf
+import device_mock
+
+
+class TestDeviceMock(unittest.TestCase):
+    def test_devices_create(self):
+        devices = device_mock.create_devices()
+        assert devices, 'got: {}'.format(devices)
+        assert 'dev1' in devices
+
+
+class TestDatabaseManagerPrimitive(unittest.TestCase):
+    def tearDown(self):
+        try:
+            os.remove(cnf.path_db)
+        except FileNotFoundError:
+            print('FileNotFoundError.', cnf.path_db, 'not exists')
+    def test_create_table(self):
+        db_manage = db_manager.DatabaseManager(cnf.test_database_name)
+        test_table_name = 'test_table'
+        test_table_columns = ['param1', 'param2']
+        db_manage.create_table(test_table_name, test_table_columns)
+
+        existing_table_names = []
+        for table in db_manage.metadata.sorted_tables:
+            existing_table_names.append(table.name)
+        existing_table_names.sort()
+        exp_table_names = ['test_table']
+
+        assert existing_table_names == exp_table_names, 'cant create db.\n' \
+            ' tables existing={}, expected={}'.format(existing_table_names,
+                                                      exp_table_names)
+        table_columns = list(db_manage.metadata.tables['test_table'].columns.keys())
+        exp_table_columns = ['time']
+        exp_table_columns.extend(test_table_columns)
+        assert table_columns == exp_table_columns, \
+                'got: {} exp:{}'.format(table_columns, exp_table_columns)
 
 
 class TestDatabaseManager(unittest.TestCase):
 
     def setUp(self):
         self.db_manage = db_manager.DatabaseManager(cnf.test_database_name)
+        self.devices = device_mock.create_devices()
+        self.db_manage.create_db(self.devices)
     # for every test separated
     def tearDown(self):
-        os.remove(cnf.path_db)
+        try:
+            os.remove(cnf.path_db)
+        except FileNotFoundError:
+            print('FileNotFoundError.', cnf.path_db, 'not exists')
 
     # @classmethod
     # def setUpClass(cls):
@@ -29,95 +69,68 @@ class TestDatabaseManager(unittest.TestCase):
     # def tearDownClass(self):
 
     def test_create_db(self):
-        print("\n\n\ntest_create_db")
-        for table_name in cnf.table_names:
-            self.db_manage.create_db(table_name, cnf.parameters)
-
-        # engine = create_engine(cnf.test_database_name)
         existing_table_names = []
         for table in self.db_manage.metadata.sorted_tables:
             existing_table_names.append(table.name)
-        existing_table_names.sort()
-        exp_table_names = sorted(cnf.table_names)
-        assert existing_table_names == exp_table_names, 'cant create'\
-        'db. tables existing={}, expected={}'.format(existing_table_names,
-                                                         exp_table_names)
 
+        existing_table_names.sort()
+        exp_table_names = sorted(cnf.dev_names)
+
+        assert existing_table_names == exp_table_names, 'cant create db.\n' \
+            ' tables existing={}, expected={}'.format(existing_table_names,
+                                                      exp_table_names)
 
     def test_read_table_from_db(self):
-        print("\n\n\ntest_read_table_from_db")
-        for table_name in cnf.table_names:
-            self.db_manage.create_db(table_name, cnf.parameters)
+        dev_name = cnf.dev_names[0]
+        table = self.db_manage.read_table_from_db(dev_name)
+        assert table is not None
 
-        table_name = cnf.table_names[0]
-        table = self.db_manage.read_table_from_db(table_name)
-        # print(type(table), '\t', table)
-        existing_namecolumns = []
-        existing_tablename = []
-        if table != None:
-            print("table_columns=", table.columns)
-            print("table_columns=", table.name)
-            existing_tablename = table.name
-            existing_namecolumns = (table.columns.keys())
-
-        namecolumns = []
-        for param in cnf.parameters:
-            namecolumns.append(param[0])
-
+        existing_tablename = table.name
+        existing_namecolumns = table.columns.keys()
+        namecolumns = ['time']
+        namecolumns.extend(cnf.dev_parameters[dev_name])
+        # TODO: split asserts to 2.
         assert sorted(namecolumns) == sorted(existing_namecolumns) and \
-            existing_tablename == table_name, \
+            existing_tablename == dev_name, \
             'cant read db tables\n existing name={} namecolumns={}\n ' \
             'expected name={} namecolums={}'.\
             format(existing_tablename, existing_namecolumns,
-                table_name, namecolumns)
-        # print(sorted(namecolumns), '\n', sorted(existing_namecolumns), '\n', existing_tablename, '\n', table_name )
+                   dev_name, namecolumns)
 
     def test_read_all_tables_from_db(self):
-        for table_name in cnf.table_names:
-            self.db_manage.create_db(table_name, cnf.parameters)
-        print("\n\n\ntest_read_all_table_from_db")
         self.db_manage.read_all_table_from_db()
+
         all_table_name = []
         for table in self.db_manage.metadata.tables:
-            print("table_name=", table)
             all_table_name.append(table)
 
-        assert sorted(all_table_name) == sorted(cnf.table_names),\
-        'cant read db\n exists table name={}\n expected table_names={}'.format(
-            all_table_name, cnf.table_names)
-
-    #
+        assert sorted(all_table_name) == sorted(cnf.dev_names),\
+            'cant read all tables db\n exists table name={}\n expected table_names={}'\
+            .format(all_table_name, cnf.dev_names)
 
     def test_insert(self):
-        print("\n\n\ntest_insert")
-        for table_name in cnf.table_names:
-            self.db_manage.create_db(table_name, cnf.parameters)
-        column_values_dict = {column_name_and_type[0]: val
-                               for column_name_and_type, val in
-                               zip(cnf.parameters, cnf.values)}
-        # column_values = dict(zip((column_name_and_type[0]
-        #                           for column_name_and_type in cnf.parameters),
-        #                          cnf.values))
-        print('column_values_dict=', column_values_dict)
-        table_name = cnf.table_names[0]
-        self.db_manage.insert(table_name, column_values_dict)
-
+        dev_name = cnf.dev_names[0]
+        column_values_dict = {param: value for param, value in zip(cnf.dev_parameters[dev_name],
+                                                                   cnf.test_values)}
+        column_values_dict['time'] = int(time.time())
+        self.db_manage.insert(dev_name, column_values_dict)
 
         # get object table with name table_name
-        table = self.db_manage.metadata.tables[table_name]
-        # print(self.db_manage.metadata.tables)
+        table = self.db_manage.metadata.tables[dev_name]
+        # connect to database
         conn = self.db_manage.engine.connect()
         # get data from table
-        list_rows_with_data = conn.execute(select([table]))
-        for row in list_rows_with_data:
+        rows_with_data = conn.execute(select([table]))
+        for row in rows_with_data:
             print(row)
 
-        list_rows_with_data.close()
+        rows_with_data.close()
         conn.close()
-
-        assert list(row) == cnf.values, \
-            'not equal expected data and ' \
-            'exists data{}\n expected data{}'.format(list(row), cnf.values)
+        param_values = list(row)
+        param_values = param_values[1:]
+        assert param_values == cnf.test_values, \
+            'Insert. Not equal exists data:{} expected data:{}'\
+            .format(param_values, cnf.test_values)
 
 
 if __name__ == '__main__':
