f04726fe4493bbb2264497391fc7914b419736e8
Artem Pilkevich <fizmat39reg.bot@mail.ru>
2014-12-12 15:55:47 +0300

    пятая версия изменена структура параметров

--- NMS_db_alch_after_D/settings.py
+++ NMS_db_alch_after_D/settings.py
@@ -4,7 +4,21 @@
 
 from sqlalchemy import Integer, String
 
-table_names = ['dev1', 'dev2', 'dev3']
-parameters = [['param1', Integer], ['param2', String], ['param3', Integer]]
-values = [1, 2, 3]
-test_database_name = 'sqlite:///databases/test.db'
+dev_names = ['dev1', 'dev2', 'dev3']
+
+parameters = {'time': Integer,
+              'param1': Integer,
+              'param2': String,
+              'param3': Integer,
+              'param4': Integer,
+              'param5': String,
+              }
+
+dev_parameters = {'dev1': ['param1', 'param2', 'param3'],
+                  'dev2': ['param4', 'param5', 'param3'],
+                  'dev3': ['param1', 'param2', 'param3']}
+
+test_values = [1, '2', 3]
+type_db = 'sqlite:///'
+path_db = 'databases/test.db'
+test_database_name = type_db + path_db
